a07a0dfdc8b503321baed7fee112912b61c0f13c
Justin Vietz <justin.vietz@eventim.de>
2019-12-18 12:58:36 +0100

    Removed unused import

--- bin/main.dart
+++ bin/main.dart
@@ -1,9 +1,9 @@
 import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 import 'package:cli/commands/add/add_command.dart';
+import 'package:cli/commands/list/list_command.dart';
 import 'package:cli/commands/run/run_command.dart';
 import 'dart:io';
-import 'package:cli/extensions/FileSystemEntityExtension.dart';
 
 const _tool_name = 'deplojer';
 
@@ -11,7 +11,8 @@ void main(List<String> args) {
 
   var runner = CommandRunner(_tool_name, 'Easy deployment of all your dotfiles')
       ..addCommand(AddCommand())
-      ..addCommand(RunCommand());
+      ..addCommand(RunCommand())
+      ..addCommand(ListCommand());
 
   if(args.isEmpty) runner.printUsage();
 
