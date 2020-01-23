7d400ea0672ed3b9580fc76a8b1d23f5e9c4781f
Justin <kiesman@pop-os.localdomain>
2020-01-23 11:13:46 +0100

    Added documentation and possibility to add external config File

--- lib/service/config_service.dart
+++ lib/service/config_service.dart
@@ -9,9 +9,9 @@ import 'package:yaml/yaml.dart';
 /// file and expose it's values.
 class ConfigService {
 
-  ConfigService(){
+  ConfigService({File configFile}){
     var confDir = Directory('platforms/');
-    var confFile = File('${confDir.absolute.path}/config.yaml');
+    var confFile = configFile ?? File('${confDir.absolute.path}/config.yaml');
     YamlMap yaml = loadYaml(confFile.readAsStringSync());
     // check if yaml is not empty
     if(yaml != null && yaml.isNotEmpty){
@@ -24,6 +24,10 @@ class ConfigService {
   /// This field holds all available platformconfigurations
   final HashMap<String, PlatformConfig> _configurations = HashMap();
 
+  /// This function will return the configuration for the given
+  /// [platform].
+  /// If the platform has **no** configuration in the config.yaml
+  /// file it'll return [PlatformConfig.empty(platform)]
   PlatformConfig configuration(String platform) {
     if(_configurations[platform] == null){
       // return empty configuration, because there is no config yet
