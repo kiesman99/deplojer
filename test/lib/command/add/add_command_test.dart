de97b661a630278b4f69ec366271fadcd0c0ec5b
Justin <kiesman@pop-os.localdomain>
2020-01-24 06:29:44 +0100

    Added some more tests

--- test/lib/command/add/add_command_test.dart
+++ test/lib/command/add/add_command_test.dart
@@ -49,5 +49,56 @@ void main() {
       expect(master_scripts_dir.existsSync(), true);
     });
 
+    test('check setup of config file', (){
+      var configFile = File('${platformDir.absolute.path}/config.yaml');
+      expect(configFile.existsSync(), true);
+    });
+
+    test('add another platform', (){
+      // another platform
+      runner.run(['add', 'windows']);
+
+      var linux_main_dir = Directory('${platformDir.absolute.path}/linux');
+      var linux_files_dir = Directory('${linux_main_dir.absolute.path}/files');
+      var linux_scripts_dir = Directory('${linux_main_dir.absolute.path}/scripts');
+      var linux_gap_filler_dir = Directory('${linux_main_dir.absolute.path}/gap_filler');
+
+      expect(linux_main_dir.existsSync(), true);
+      expect(linux_files_dir.existsSync(), true);
+      expect(linux_scripts_dir.existsSync(), true);
+      expect(linux_gap_filler_dir.existsSync(), true);
+
+      var windows_main_dir = Directory('${platformDir.absolute.path}/windows');
+      var windows_files_dir = Directory('${linux_main_dir.absolute.path}/files');
+      var windows_scripts_dir = Directory('${linux_main_dir.absolute.path}/scripts');
+      var windows_gap_filler_dir = Directory('${linux_main_dir.absolute.path}/gap_filler');
+
+      expect(windows_main_dir.existsSync(), true);
+      expect(windows_files_dir.existsSync(), true);
+      expect(windows_scripts_dir.existsSync(), true);
+      expect(windows_gap_filler_dir.existsSync(), true);
+    });
+
+    test('setup platform twice', (){
+      var linux_main_dir = Directory('${platformDir.absolute.path}/linux');
+      var linux_files_dir = Directory('${linux_main_dir.absolute.path}/files');
+      var linux_scripts_dir = Directory('${linux_main_dir.absolute.path}/scripts');
+      var linux_gap_filler_dir = Directory('${linux_main_dir.absolute.path}/gap_filler');
+
+      var linux_sample_file = File('${linux_files_dir.absolute.path}/testfile')
+        ..createSync();
+      
+      expect(linux_sample_file.existsSync(), true);
+
+      runner.run(['add', 'linux']);
+
+      expect(linux_main_dir.existsSync(), true);
+      expect(linux_files_dir.existsSync(), true);
+      expect(linux_scripts_dir.existsSync(), true);
+      expect(linux_gap_filler_dir.existsSync(), true);
+      expect(linux_sample_file.existsSync(), true);
+
+    });
+
   });
 }
