2aa3afc7eba2c08c528ea67d3e3272255703fff4
Justin <kiesman@pop-os.localdomain>
2020-01-24 08:16:20 +0100

    Renamed file

---
+++ test/lib/command/list/list_command_test.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:args/command_runner.dart';
+import 'package:cli/commands/list/list_command.dart';
+import 'package:test/test.dart';
+import 'package:cli/extensions/string_extension.dart';
+
+void main() {
+
+  CommandRunner<String> runner;
+  Directory platformDir;
+
+  setUp(() {
+    runner = CommandRunner('deplojer', 'A simple test')
+      ..addCommand(ListCommand());
+
+    platformDir = Directory('platforms')
+      ..createSync();
+
+    // setup some platforms
+    Directory('${platformDir.absolute.path}/linux').createSync();
+    Directory('${platformDir.absolute.path}/windows').createSync();
+    Directory('${platformDir.absolute.path}/mac').createSync();
+    Directory('${platformDir.absolute.path}/PopOs!').createSync();
+  });
+
+  tearDown(() {
+    //if(platformDir.existsSync()) platformDir.deleteSync(recursive: true);
+  });
+
+  test('basictest', () async {
+
+    var res = await runner.run(['list']);
+
+    print(res);
+    expect(res, equals(
+'''
+windows
+linux
+PopOs!
+mac
+'''));
+  });
+
+
+}
